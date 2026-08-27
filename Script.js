e10b1815cc31e4964340a5851c8c018d16c17bea
Aniket kantarao kharad <kharadaniket96@gmail.com>
2026-08-27 15:16:22 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,26 @@
+console.log("Aniket Kharat")
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+// boxes[3].style.backgroundColor = "green"
+// boxes[4].style.color = "bl
+document.getElementById("greenbox").style.backgroundColor = "green"
+
+// document.getElementById("pink").style.backgroundColor = "pink"
+
+// document.querySelector(".box").style.backgroundColor = "green";
+// console.log(document.querySelectorAll(".box"))
+
+// document.querySelectorAll(".box").forEach(e =>{
+//     e.style.backgroundColor = "green";
+// })
+
+// console.log(document.querySelectorAll(".box"))
+
+//  document.querySelectorAll(".box").forEach(e =>{
+//      e.style.background = "red"
+
+//  })
+
+document.getElementById("redbox").style.backgroundColor = "red"
+document.getElementById("pinkbox").style.backgroundColor = "pink"
+document.getElementById("yellowbox").style.backgroundColor = "yellow"
---
+++ main.js
@@ -0,0 +1,5 @@
+function createCard(title,cName,monthOld, duration){
+    //  Finish ththis  Function
+}
+
+createCard("Introduction to Backend | Sigma Web Dev video #2","Codewithharry")
